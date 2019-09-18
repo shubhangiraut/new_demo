df452915c0fc3e224af0c829c46c380eda5eef51
shubhangiraut <rautss1234@gmail.com>
2019-09-18 19:46:02 +0530

    Added Requirement.txt

--- drfx-feature/event/admin.py
+++ drfx-feature/event/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from models import Event
+# from __future__ import unicode_literals
+# from models import Event
  
 class EventAdmin(admin.ModelAdmin):
     list_display = ['day', 'start_time', 'end_time', 'notes']
